f118b65016b221d47a0af0612d8ad86e933b2b72
mig21bb <mig21bb@gmail.com>
2017-05-02 15:57:00 +0200

    Correción inserts
    Hay que especificar el esquema weathernow.   antes de cada tabla.

--- sky-sql-inserts.sql
+++ sky-sql-inserts.sql
@@ -2,10 +2,10 @@
 -- Query: SELECT * FROM weathernow.estadoscielo
 -- Date: 2017-04-27 00:02
 */
-INSERT INTO `estadoscielo` (`id_est`,`estado`) VALUES (1,'despejado');
-INSERT INTO `estadoscielo` (`id_est`,`estado`) VALUES (2,'nuves y claros');
-INSERT INTO `estadoscielo` (`id_est`,`estado`) VALUES (3,'nublado');
-INSERT INTO `estadoscielo` (`id_est`,`estado`) VALUES (4,'niebla');
-INSERT INTO `estadoscielo` (`id_est`,`estado`) VALUES (5,'nublado');
-INSERT INTO `estadoscielo` (`id_est`,`estado`) VALUES (6,'lluvia');
-INSERT INTO `estadoscielo` (`id_est`,`estado`) VALUES (7,'tormenta');
+INSERT INTO weathernow.`estadoscielo` (`id_est`,`estado`) VALUES (1,'despejado');
+INSERT INTO weathernow.`estadoscielo` (`id_est`,`estado`) VALUES (2,'nuves y claros');
+INSERT INTO weathernow.`estadoscielo` (`id_est`,`estado`) VALUES (3,'nublado');
+INSERT INTO weathernow.`estadoscielo` (`id_est`,`estado`) VALUES (4,'niebla');
+INSERT INTO weathernow.`estadoscielo` (`id_est`,`estado`) VALUES (5,'nublado');
+INSERT INTO weathernow.`estadoscielo` (`id_est`,`estado`) VALUES (6,'lluvia');
+INSERT INTO weathernow.`estadoscielo` (`id_est`,`estado`) VALUES (7,'tormenta');
